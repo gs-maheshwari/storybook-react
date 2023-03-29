76a079b64ba834a92965767d0dc90f3ceeff353e
Gourishankar Bhutra <er.gourishankar@gmail.com>
2023-03-29 07:51:16 +0200

    feat: update

--- src/components/Inbox.stories.tsx
+++ src/components/Inbox.stories.tsx
@@ -3,7 +3,9 @@ import Inbox from './Inbox';
 import store from '../lib/store';
 
 import { Provider } from 'react-redux';
-import { ComponentMeta } from '@storybook/react';
+import { ComponentMeta, Story } from '@storybook/react';
+import { rest } from 'msw';
+import { MockedState } from './TaskList.stories';
 
 export default {
   component: Inbox,
@@ -13,5 +15,26 @@ export default {
 
 const Template = () => <Inbox />;
 
-export const Default = Template.bind({});
-export const Error = Template.bind({});
+export const Default: Story = Template.bind({});
+
+Default.parameters  = {
+    msw: {
+        handlers: [
+            rest.get('https://jsonplaceholder.typicode.com/todos?userId=1', (req, res, ctx) => {
+                return res(ctx.json(MockedState.tasks))
+            })
+        ]
+    }
+}
+
+export const Error: Story = Template.bind({});
+
+Error.parameters = {
+    msw: {
+        handlers: [
+            rest.get('https://jsonplaceholder.typicode.com/todos?userId=1', (req, res, ctx) => {
+                return res(ctx.status(403))
+            })
+        ]
+    }
+}
